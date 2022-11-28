c672264415c59382f9da9d3809eaa51be036f4e6
hamidaya <info@ayachi.com>
2022-11-28 10:09:24 +0100

    tested

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log('Hallo daar!');
+// import * as url from "url";
+import axios from 'axios';
+
+async function fetchCountryData() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/all');
+        const countries = result.data;
+       // sorteer de huidige data array op de populatie-property van elk land
+ //       countries.sort((a, b) => {
+ //       return a.population - b.population;
+ //                  });
+        console.log(countries)
+
+        // geef de gesorteerde data array mee aan de functie die de elementen op de pagina injecteert
+ //       createListItems(countries);
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+fetchCountryData();
